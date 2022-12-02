e73101e42efcc8a9269e19620df059a7ddd17dbb
Oliver Pearce <oliver.pearce@husqvarnagroup.com>
2022-12-02 16:37:18 +0100

    feat: initial commit

---
+++ main.ts
@@ -0,0 +1,20 @@
+import { copy } from "https://deno.land/std@0.167.0/streams/conversion.ts";
+import { readline } from "https://deno.land/x/readline@v1.1.0/mod.ts";
+
+const filename = 'elvefood';
+const file = await Deno.open(filename);
+const foods = [];
+let currentFoodCount = 0;
+for await (const line of readline(file)) {
+  const decodedLine = new TextDecoder().decode(line);
+  if (decodedLine !== '') {
+    currentFoodCount += parseInt(decodedLine);
+  } else {
+    foods.push(currentFoodCount);
+    currentFoodCount = 0;
+  }
+}
+
+//foods.forEach(food => console.log(food));
+
+console.info(Math.max(...foods));
